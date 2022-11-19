84e15cbd928e67a8b784424abeb78d9dee8824b6
Eduardo Valdes Inerarte <eddremonts86@gmail.com>
2022-11-19 06:09:09 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+      <v-card class="ma-5">
+        <v-card-title>Vuetify</v-card-title>
+        <v-card-text>First Card</v-card-text>
+      </v-card>
+
+      <v-card class="ma-5">
+        <v-card-title>Vuetify</v-card-title>
+        <v-card-text>First Card</v-card-text>
+      </v-card>
+
+      <v-card class="ma-5">
+        <v-card-title>Vuetify</v-card-title>
+        <v-card-text>First Card</v-card-text>
+      </v-card>
+
+  </v-container>
+</template>
